7a555efa0ff0ab3086ef15b284a807f65838d498
Joost Evertse <joustie@gmail.com>
2023-05-31 16:03:48 +0200

    Renamed attributes

--- models.go
+++ models.go
@@ -4,11 +4,11 @@ package leostream
 type Center struct {
 	ID           int                `json:"id"`
 	Name         string             `json:"name"`
-	os			 string             `json:"os"`
-	flavor		 string             `json:"flavor"`
-	online       int                `json:"online"`
-	status       int                `json:"status"`
-	status_label string             `json:"status_label"`
-	center_type  string             `json:"type"`
-	type_label   string             `json:"type_label"`
+	Os			 string             `json:"os"`
+	Flavor		 string             `json:"flavor"`
+	Online       int                `json:"online"`
+	Status       int                `json:"status"`
+	Status_label string             `json:"status_label"`
+	Center_type  string             `json:"type"`
+	Type_label   string             `json:"type_label"`
 }
