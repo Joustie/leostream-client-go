7c5bbcc46cdd611bf71743d927cc5b8012db97b5
Joost Evertse <joustie@gmail.com>
2023-12-08 18:06:06 +0100

    Added debug

--- poolassignment.go
+++ poolassignment.go
@@ -66,6 +66,7 @@ func (c *Client) CreatePoolAssignment(poolassignment PoolAssignment, policyID in
 
 	println("policyID: ", policyID)
 	fmt.Printf("%+v\n", poolassignment)
+	fmt.Printf("%+v\n", string(rb))
 	/*req, err := http.NewRequest("POST", fmt.Sprintf("%s/policies/%s/pool-assignments", c.HostURL, policyID), strings.NewReader(string(rb)))
 	if err != nil {
 		return nil, err
