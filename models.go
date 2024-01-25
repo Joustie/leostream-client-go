a46d900147be6dd93f4645eaa4d7b35d355a1924
Joost Evertse <joustie@gmail.com>
2024-01-25 13:17:25 +0100

    Verified more attributes for pools to work

--- models.go
+++ models.go
@@ -88,49 +88,51 @@ type GatewaysStored struct {
 
 type Provision struct {
 	Provision_on_off             int64            `json:"provision_on_off"`
-	Provision_tenant_id          int64            `json:"provision_tenant_id,omitempty"`
+	Provision_tenant_id          int64            `json:"provision_tenant_id"`
 	Provision_threshold          int64            `json:"provision_threshold"`
-	Provision_vm_id              int64            `json:"provision_vm_id,omitempty"`
-	Provision_vm_name            string           `json:"provision_vm_name,omitempty"`
-	Provision_vm_name_next_value int64            `json:"provision_vm_name_next_value,omitempty"`
-	Provision_vm_display_name    string           `json:"provision_vm_display_name,omitempty"`
-	Provision_url                string           `json:"provision_url,omitempty"`
-	Provision_server_id          int64            `json:"provision_server_id,omitempty"`
+	Provision_vm_id              int64            `json:"provision_vm_id"`
+	Provision_vm_name            string           `json:"provision_vm_name"`
+	Provision_vm_name_next_value int64            `json:"provision_vm_name_next_value"`
+	Provision_vm_display_name    string           `json:"provision_vm_display_name"`
+	Provision_url                string           `json:"provision_url"`
+	Provision_server_id          int64            `json:"provision_server_id"`
 	Provision_max                int64            `json:"provision_max"`
-	Provision_limits_enforce     int64            `json:"provision_limits_enforce,omitempty"`
-	Mark_deleteable    			 int64            `json:"mark_deleteable,omitempty"`
-	Mark_unavailable   			 int64            `json:"mark_unavailable,omitempty"`
-	Time_limits        			 []PoolTimeLimits `json:"time_limits,omitempty"`
-	Center             			 PoolAwsCenter    `json:"center,omitempty"`
+	Provision_limits_enforce     int64            `json:"provision_limits_enforce"`
+	Mark_deleteable    			 int64            `json:"mark_deletable"`
+	Mark_unavailable   			 int64            `json:"mark_unavailable"`
+	Time_limits        			 *[]PoolTimeLimits `json:"time_limits,omitempty"`
+	Center             			 *PoolAwsCenter    `json:"center,omitempty"`
 }
 
 type PoolAwsCenter struct {
-	ID               int64  `json:"id,omitempty"`
-	Name             string `json:"name,omitempty"`
-	Type             string `json:"type,omitempty"`
-	Aws_size         string `json:"aws_size,omitempty"`
-	aws_t2_unlimited int64  `json:"aws_t2_unlimited"`
-	aws_iam_name     string `json:"aws_iam_name,omitempty"`
-	aws_sub_net      string `json:"aws_sub_net,omitempty"`
-	aws_sec_group    string `json:"aws_sec_group,omitempty"`
-	aws_vpc_id       string `json:"aws_vpc_id,omitempty"`
+	ID               	int64  	`json:"id,omitempty"`
+	Name             	string 	`json:"name,omitempty"`
+	Type             	string 	`json:"type,omitempty"`
+	Aws_size         	string 	`json:"aws_size,omitempty"`
+	Aws_t2_unlimited 	int64  	`json:"aws_t2_unlimited,omitempty"`
+	Aws_iam_name     	string 	`json:"aws_iam_name,omitempty"`
+	Aws_sub_net      	string 	`json:"aws_sub_net,omitempty"`
+	Aws_sec_group    	string 	`json:"aws_sec_group,omitempty"`
+	Aws_vpc_id       	string 	`json:"aws_vpc_id,omitempty"`
+	Vc_resource_pool_id int64  	`json:"vc_resource_pool_id,omitempty"`
+	Vc_spec_file_id	 	int64  	`json:"vc_spec_file_id,omitempty"`
 }
 
 // Pool summary
 type PoolSummary struct {
 	ID           int64  `json:"id,omitempty"`
 	Name         string `json:"name,omitempty"`
-	assigned_vm  int64  `json:"assigned_vm,omitempty"`
-	available_vm int64  `json:"available_vm,omitempty"`
-	unavailable_vm int64  `json:"unavailable_vm,omitempty"`
-	total_vm    int64  `json:"total_vm,omitempty"`
-	parent_pool_id int64  `json:"parent_pool_id,omitempty"`
-	total_agent_running int64  `json:"total_agent_running,omitempty"`
-	total_connected int64  `json:"total_connected,omitempty"`
-	total_logged_in int64  `json:"total_logged_in,omitempty"`
-	total_vm_running int64  `json:"total_vm_running,omitempty"`
-	total_vm_stopped int64  `json:"total_vm_stopped,omitempty"`
-	total_vm_suspended int64  `json:"total_vm_suspended,omitempty"`
+	Assigned_vm  int64  `json:"assigned_vm,omitempty"`
+	Available_vm int64  `json:"available_vm,omitempty"`
+	Unavailable_vm int64  `json:"unavailable_vm,omitempty"`
+	Total_vm    int64  `json:"total_vm,omitempty"`
+	Parent_pool_id int64  `json:"parent_pool_id,omitempty"`
+	Total_agent_running int64  `json:"total_agent_running,omitempty"`
+	Total_connected int64  `json:"total_connected,omitempty"`
+	Total_logged_in int64  `json:"total_logged_in,omitempty"`
+	Total_vm_running int64  `json:"total_vm_running,omitempty"`
+	Total_vm_stopped int64  `json:"total_vm_stopped,omitempty"`
+	Total_vm_suspended int64  `json:"total_vm_suspended,omitempty"`
 }
 
 
@@ -141,30 +143,33 @@ type Pool struct {
 	Display_name               string         `json:"display_name,omitempty"`
 	Notes                      string         `json:"notes,omitempty"`
 	Running_desktops_threshold int64          `json:"running_desktops_threshold,omitempty"`
-	Pool_definition            PoolDefinition `json:"pool_definition,omitempty"`
-	Provision	               Provision 	  `json:"provision,omitempty"`
+	Pool_definition            *PoolDefinition `json:"pool_definition,omitempty"`
+	Provision	               *Provision 	  `json:"provision,omitempty"`
+	PoolStats				  *PoolStats     `json:"pool_stats,omitempty"`
+	Pool_domain                *PoolDomain    `json:"pool_domain,omitempty"`
+	Pool_log                   *PoolLog       `json:"pool_log,omitempty"`
 }
 
 type PoolDefinition struct {
 	Restrict_by             string           `json:"restrict_by,omitempty"`
-	// Vm_tags                 []string         `json:"vm_tags,omitempty"`
-	// Vm_tags_join            string           `json:"vm_tags_join,omitempty"`
-	// Pool_attribute_join     string           `json:"pool_attribute_join,omitempty"`
-	// Adhoc_vms               []int64          `json:"adhoc_vms,omitempty"`
-	// Adhoc_vc_hosts          []int64          `json:"adhoc_vc_hosts,omitempty"`
-	// Adhoc_vc_clusters       []int64          `json:"adhoc_vc_clusters,omitempty"`
-	// Adhoc_vc_resource_pools []int64          `json:"adhoc_vc_resource_pools,omitempty"`
+	Vm_tags                 []string         `json:"vm_tags,omitempty"`
+	Vm_tags_join            string           `json:"vm_tags_join,omitempty"`
+	Pool_attribute_join     string           `json:"pool_attribute_join,omitempty"`
+	Adhoc_vms               []int64          `json:"adhoc_vms,omitempty"`
+	Adhoc_vc_hosts          []int64          `json:"adhoc_vc_hosts,omitempty"`
+	Adhoc_vc_clusters       []int64          `json:"adhoc_vc_clusters,omitempty"`
+	Adhoc_vc_resource_pools []int64          `json:"adhoc_vc_resource_pools,omitempty"`
 	Never_rogue             int64            `json:"never_rogue"`
 	Server_ids              []int64          `json:"server_ids,omitempty"`
-	Use_vmotion             int64            `json:"use_vmotion"`
-	// Vm_server_distribution  int64            `json:"vm_server_distribution,omitempty"`
-	// Attributes              []PoolAttributes `json:"attributes,omitempty"`
+    Use_vmotion             int64            `json:"use_vmotion"`
+	Vm_server_distribution  int64            `json:"vm_server_distribution,omitempty"`
+	Attributes              []PoolAttributes `json:"attributes,omitempty"`
 }
 
 type PoolAttributes struct {
 	Vm_table_field     string `json:"vm_table_field,omitempty"`
-	Ad_attribute_field string `json:"ad_attribute_field,omitempty"`
-	Vm_gpu_field       string `json:"vm_gpu_field,omitempty"`
+	Ad_attribute_field string `json:"ad_attribute_field"`
+	Vm_gpu_field       string `json:"vm_gpu_field"`
 	Text_to_match      string `json:"text_to_match,omitempty"`
 	Condition_type     string `json:"condition_type,omitempty"`
 }
